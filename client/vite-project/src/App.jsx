2fed837d8eaf557c844e22d1d193068f63280a51
David <doladepo128@gmail.com>
2023-07-01 14:47:04 +0100

    imported tailwind and chartjs libraries

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Graph from './components/Graph';
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -9,7 +9,16 @@ function App() {
   return (
     <>
       <div className='App'>
-        <h1>Expense Tracker</h1>
+        <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+
+        {/* grid columns */}
+        <div className='grid md:grid-cols-2 gap-4'>
+         {/* Chart */}
+         <Graph></Graph>
+         {/* Form */}    
+        </div>
+        </div>
       </div>
   
     </>

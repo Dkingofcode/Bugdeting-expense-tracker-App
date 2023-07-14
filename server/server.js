3f29c1df7a78063443e7769c3001ecb157cba8fd
David <doladepo128@gmail.com>
2023-07-14 13:02:11 +0100

    Finished backend api and added Reduxtool-kit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+require('dotenv').config({ path: "./config.env"});
+const port = process.env.PORT || 5000;
+//const port = 5000;
+
+//use middleware
+app.use(cors());
+app.use(express.json());
+
+const con = require('./db/connection');
+
+app.use(require("./routes/route"));
+
+app.listen(port, () => {
+  console.log(`Server running on port: http://localhost:${port}`);  
+})
+
